f5165184df2a0da05bd72984cb97e7e8f70b274a
Jonathan <jolafrite@users.noreply.github.com>
2023-09-05 01:33:11 +0800

    Update apps.sh

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -19,7 +19,21 @@ apps=(
 	zoom
 )
 
+mas_apps=(
+  "937984704"   # Amphetamine
+  "1444383602"  # Good Notes 5
+  "768053424"   # Gappling (svg viewer)
+  "1352778147"  # Bitwarden
+)
+
 install_macos_apps() {
 	info "Installing macOS apps..."
 	install_brew_casks "${apps[@]}"
+}
+
+install_masApps() {
+	info "Installing App Store apps..."
+	for app in "${mas_apps[@]}"; do
+		mas install "$app"
+	done
 }
